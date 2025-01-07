88d5e501b102919779e59db5984b48fecd85938d
Earl <earlwmanley@maximus.com>
2025-01-07 18:37:52 -0500

    First commit

--- src/main/java/com/battlefortheorbs/bfto/BftoApplication.java
+++ src/main/java/com/battlefortheorbs/bfto/BftoApplication.java
@@ -8,7 +8,7 @@ public class BftoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BftoApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello! World");
 	}
 
 }
