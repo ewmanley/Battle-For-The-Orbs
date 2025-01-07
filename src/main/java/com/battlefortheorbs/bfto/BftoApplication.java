fc5399583e4347d80feb099cd05577290a61e2dc
Earl <earlwmanley@maximus.com>
2025-01-07 18:01:30 -0500

    first commit

---
+++ src/main/java/com/battlefortheorbs/bfto/BftoApplication.java
@@ -0,0 +1,14 @@
+package com.battlefortheorbs.bfto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BftoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BftoApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/battlefortheorbs/bfto/BftoApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.battlefortheorbs.bfto;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BftoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
